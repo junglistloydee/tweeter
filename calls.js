d33fff81a3de606b29174188221b439b4ede786f
Lloyd <lloyd@ldkdirect.co.uk>
2018-04-19 22:25:23 +0100

    reduced code size and created new calls
    friendships/create, statuses/retweet and favorites/create has been added

--- calls.js
+++ calls.js
@@ -13,47 +13,44 @@ var parms = {
 // min number of likes
 var rate = 100;
 
-var call = tweet.get('search/tweets', parms, function (err, data, response) { // search for the tweets with the stored #hashtag
+var call = tweet.get('search/tweets', parms, function (err, data, response) {
     if (!err) {
-        for (let i = 0; i < data.statuses.length; i++) { // we want to check all returned tweets
-            let id = { id: data.statuses[i].id_str }
-            let user = { user: data.statuses[i].user.id }
-            if (data.statuses.favorite_count[i] > rate) { // only get tweets with the stored rate
-                T.post('favorites/create', id, function (err, response) {
-                    if (err) { // log any errors
+        for (let i = 0; i < data.statuses.length; i++) {
+            // sets the ids to be used when making the call
+            let favId = { id: data.statuses[i].id_str }
+            let folId = { user: data.statuses[i].user.id }
+            let retId = { id: data.statuses[i].id_str }
+            if (data.statuses[i].favourites > rate) {
+                T.post('favorites/create', favId, function (err, response) { // favourite the tweet
+                    if (err) {
                         console.log(err[0].message);
                     }
-                    else { // follow user, log and store the tweet
-                        T.post('friendships/create', user, function (err, response) {
-                            if (err) {
-                                console.log(err[0].message);
-                            }
-                            else {
-                                let userid = response.user.user_id;
-                                let friends = response.user.friends;
-                                console.log('Friended: ' + userid + ' Count: ' + friends);
-                            }
-                            // grab ID of tweet to retweet
-                            var retweetId = data.statuses[i].id_str;
-                            T.post('statuses/retweet/:id', { // retweet the tweet
-                                id: retweetId
-                            }, function (err, response) {
-                                if (response) {
-                                    console.log('Retweeted: ' + retweetId);
-                                }
-                                if (err) {
-                                    console.log('Unable to retweet.');
-                                }
-                            });
-                        });
+                    else {
                         let username = response.user.screen_name;
                         let tweetId = response.id_str;
-                        let fav = response.favorite_count;
-                        // TODO: Add data to database so 'best tweets' can be reused
-                        console.log('Favorited: ', `https://twitter.com/${username}/status/${tweetId}`, 'FavCount: ', fav);
-                        console.log('user: ', 'has been followed');
+                        console.log('Favorited: ', `https://twitter.com/${username}/status/${tweetId}`)
                     }
                 })
+                T.post('friendships/create', user, function (err, response) { // friend the friend
+                    if (err) {
+                        console.log(err[0].message);
+                    }
+                    else {
+                        let userid = response.user.user_id;
+                        let friends = response.user.friends;
+                        console.log('Friended: ' + userid + ' Count: ' + friends);
+                    }
+                });
+                T.post('statuses/retweet/:id', { // retweet the tweet
+                    id: retweetId
+                }, function (err, response) {
+                    if (response) {
+                        console.log('Retweeted: ' + retweetId);
+                    }
+                    if (err) {
+                        console.log('Unable to retweet.');
+                    }
+                });
             }
         }
     } else {
@@ -61,4 +58,4 @@ var call = tweet.get('search/tweets', parms, function (err, data, response) { //
     }
 })
 
-module.exports.call = call;  
+module.exports = call;
